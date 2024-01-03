4a696fe0a7e71299bea3558e6736142b61352e14
githushaji <148202806+githushaji@users.noreply.github.com>
2024-01-03 15:25:34 +0530

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,9 @@
+list=[]
+list1=({'rose','sunflower','lotus'})
+list2=({'lotus','jasmine','rose'})
+print("elements in list1=",list1)
+print("elements in list2=",list2)
+for i in list1 if not in list2:
+ print("difference of elements" =list1.difference(list2))
+print(list)
+
